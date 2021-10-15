4afb0ab115581bbbf14ba02f1f7cc3422ac48b09
Jiayu Liu <406336801@qq.com>
2021-10-15 19:57:29 +0800

    add RandomForest

--- model_zoo/__init__.py
+++ model_zoo/__init__.py
@@ -1,9 +1,11 @@
 from .base_model import BaseModel
 from .logistic_regression import LogisticRegressionModel
 from .gradientboosting_regression import GradientBoostingRegressionModel
+from .randomforest_regression import RandomForestRegressionModel
 
 __all__ = [
     'BaseModel',
     'LogisticRegressionModel',
-    'GradientBoostingRegressionModel'
+    'GradientBoostingRegressionModel',
+    'RandomForestRegressionModel'
 ]
